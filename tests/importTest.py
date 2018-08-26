88e58fe32840234649ca3949375d4c55c844e50a
jazzbin <42562919+geatpy-dev@users.noreply.github.com>
2018-08-26 15:48:04 +0800

    Update importTest.py

--- tests/importTest.py
+++ tests/importTest.py
@@ -6,4 +6,6 @@ import platform
 lib_path = __file__[:-11] + 'lib' + platform.architecture()[0][:2] + '/v' + sys.version[:3] + '/'
 if lib_path not in sys.path:
     sys.path.append(lib_path)
+print(sys.path)
+print()
 import geatpy
