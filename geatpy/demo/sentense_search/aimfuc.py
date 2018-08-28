1fd497c6eb38409680f7731a7606d2078eb0b36e
geatpy-dev <jazzbin@geatpy.com>
2018-08-28 23:37:51 +0800

    Don't try to use geatpy of the old version or you will get troubles with some copyright issues.

--- geatpy/demo/sentense_search/aimfuc.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def aimfuc(Phen):
-    real = np.array([ord('I'),ord(' '),ord('a'),ord('m'),ord(' '),ord('a'),
-                     ord(' '),ord('l'),ord('i'),ord('t'),ord('t'),ord('l'),
-                     ord('e'),ord(' '),ord('b'),ord('o'),ord('y')])
-    diff = np.sum((Phen - real)**2, 1)
-    return np.array([diff]).T
