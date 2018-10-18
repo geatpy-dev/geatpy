259ffc5ec9a434a81818604b12cff4d4fdfa6fc6
geatpy-dev <jazzbin@geatpy.com>
2018-10-18 12:01:52 +0800

    update v1.1.0

---
+++ geatpy/demo/ZDT_demo/main.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+执行脚本main.py
+描述：
+    该demo是展示如何利用Geatpy计算多目标优化标准测试函数ZDT1,2,3,4,6的帕累托前沿
+    本案例通过调用moea_q_sorted_templet算法模板来解决该多目标优化问题，
+    其详细用法可利用help命令查看，或是在github下载并查看源码
+    其中目标函数ZDT1,2,3,4,6写在aimfuc.py文件中
+"""
+
+import numpy as np
+import geatpy as ga # 导入geatpy库
+
+# 获取函数接口地址
+AIM_M = __import__('aimfuc')
+
+"""============================变量设置============================"""
+# for ZDT1-3
+ranges = np.vstack([np.zeros((1,30)), np.ones((1,30))])   # 生成自变量的范围矩阵
+borders = np.vstack([np.ones((1,30)), np.ones((1,30))])   # 生成自变量的边界矩阵
+precisions = [4] * 30 # 自变量的编码精度，由于控制变量是连续型的，并且算法模板中使用实值编码，因此这里设置成大于0的任意值即可
+AIM_F = 'ZDT1'
+
+# for ZDT4
+#ranges = np.vstack([-5 * np.ones((1,9)), 5 * np.ones((1,9))])   # 生成自变量的范围矩阵
+#ranges = np.hstack([np.array([[0],[1]]), ranges])
+#borders = np.vstack([np.ones((1,10)), np.ones((1,10))])   # 生成自变量的边界矩阵
+#precisions = [4] * 10              # 自变量的编码精度
+#AIM_F = 'ZDT4'
+
+# for ZDT6
+#ranges = np.vstack([np.zeros((1,10)), 5 * np.ones((1,10))])   # 生成自变量的范围矩阵
+#borders = np.vstack([np.ones((1,10)), np.ones((1,10))])   # 生成自变量的边界矩阵
+#precisions = [4] * 10              # 自变量的编码精度
+#AIM_F = 'ZDT6'
+
+"""========================遗传算法参数设置========================="""
+NIND = 50                 # 种群规模
+MAXGEN = 1000             # 最大遗传代数
+GGAP = 1                  # 代沟：子代与父代的重复率为(1-GGAP),由于后面使用NSGA2算法，因此该参数无用
+selectStyle = 'etour'     # 遗传算法的选择方式——带精英策略的锦标赛选择
+recombinStyle = 'xovdprs' # 遗传算法的重组方式，设为使用代理的两点交叉
+recopt = 0.9              # 交叉概率
+pm = 0.1                  # 变异概率
+SUBPOP = 1                # 设置种群数为1
+maxormin = 1              # 设置标记表明这是最小化目标
+MAXSIZE = 2000            # 帕累托最优解集中解的最大个数
+FieldDR = ga.crtfld(ranges, borders, precisions) # 生成区域描述器
+"""=======================调用编程模板进行种群进化==================="""
+# 得到帕累托最优解集NDSet以及解集对应的目标函数值NDSetObjV
+[ObjV, NDSet, NDSetObjV, times] = ga.moea_q_sorted_templet(AIM_M, AIM_F, None, None, FieldDR, 'R', maxormin, MAXGEN, MAXSIZE, NIND, SUBPOP, GGAP, selectStyle, recombinStyle, recopt, pm, distribute = True, drawing = 1)
---
+++ geatpy/demo/single_objective_demo4/main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+执行脚本main.py
+描述：
+    该demo是展示如何计算带等式约束的单目标优化问题:
+        max 4 * x1 + 2 * x2 + x3
+        s.t.
+            2 * x1 + x2 <= 1
+            x1 + 2 * x3 > 2
+            x1 + x2 + x3 == 1
+            0 <= x1 <= 1
+            0 <= x2 <= 1
+            0 <= x3 <= 2
+    其中目标函数和约束条件写在aimfuc.py文件中，适应度罚函数写在罚函数文件punishing.py中
+    本案例通过降维的方法，将等式约束化成了不等式约束，大大拓宽了可行解的空间，方便遗传算法求解
+    此外，本案例展示了利用多种群竞争的进化算法模板sga_mpc_real_templet了解决该问题。
+"""
+
+import numpy as np
+import geatpy as ga
+
+# 获取函数接口地址
+AIM_M = __import__('aimfuc')
+PUN_M = __import__('punishing')
+# 变量设置
+x1 = [0, 1] # 自变量1的范围
+x2 = [0, 1] # 自变量2的范围
+b1 = [1, 1] # 自变量1是否包含下界
+b2 = [1, 1] # 自变量2是否包含上界
+ranges=np.vstack([x1, x2]).T # 生成自变量的范围矩阵
+borders = np.vstack([b1, b2]).T # 生成自变量的边界矩阵
+precisions = [2] * 2 # 自变量的编码精度，由于控制变量是连续型的，并且算法模板中使用实值编码，因此这里设置成大于0的任意值即可
+newRanges = ga.meshrng(ranges, gridnum = 2) # 对控制变量范围进行网格化，网格边长为2
+# 生成网格化后的区域描述器集合
+FieldDRs = []
+for i in range(len(newRanges)):
+    FieldDRs.append(ga.crtfld(newRanges[i], borders, precisions))
+# 调用编程模板
+[pop_trace, var_trace, times] = ga.sga_mpc_real_templet(AIM_M, 'aimfuc', PUN_M,\
+ 'punishing', FieldDRs, problem = 'R', maxormin = -1, MAXGEN = 100, NIND = 100,\
+ SUBPOP = 1, GGAP = 0.9, selectStyle = 'etour', recombinStyle = 'xovdprs',\
+ recopt = 0.9, pm = 0.3, distribute = True, drawing = 1)
