e9d8254bbcbeb8c6929e5dddf06dbf578d1e5b57
geatpy-dev <jazzbin@geatpy.com>
2019-07-28 16:17:06 +0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ kwargs = dict(name = "geatpy",
     'Development Status :: 4 - Beta',
     'Topic :: Software Development',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python'
+    'Programming Language :: Python',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
