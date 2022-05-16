41d859db646021a5e79fd0a870102a62a8c6725a
hktkzyx <hktkzyx@yeah.net>
2022-05-16 18:31:33 +0800

    build(setup.py): clear codes and use `pathlib` instead of `os`

--- setup.py
+++ setup.py
@@ -3,81 +3,86 @@
 #
 #    Geatpy is a free toolbox: you can redistribute it and/or modify as you want.
 #
-#    Geatpy is distributed in the hope that it will be useful for the genetic 
+#    Geatpy is distributed in the hope that it will be useful for the genetic
 #    and evolutionary algorithm, you can get the tutorial from http://www.geatpy.com
 #
 #    If you want to donate to it, please email geatpy@163.com
 
-import os
-import sys
-import shutil
+import pathlib
 import platform
+import shutil
+import sys
+
+from Cython.Build import cythonize
 import numpy as np
 import setuptools
-from Cython.Build import cythonize
 from setuptools import setup
 
-LONG_DESCRIPTION = "Geatpy--The Genetic and Evolutionary Algorithms Toolbox for Python. http://www.geatpy.com"
-
-kwargs = dict(name = "geatpy",
-    version = "2.7.0",
-    description = "Geatpy is a high-performance Genetic and Evolutionary Algorithms toolbox for Python.",
-    author = "Geatpy Team",
-    author_email = "geatpy@163.com",
-    url = "http://www.geatpy.com",
+kwargs = dict(
+    name="geatpy",
+    version="2.7.0",
+    description=("Geatpy is a high-performance "
+                 "Genetic and Evolutionary Algorithms toolbox for Python."),
+    author="Geatpy Team",
+    author_email="geatpy@163.com",
+    url="http://www.geatpy.com",
     packages=setuptools.find_packages(),
-    include_package_data = True,    # Enabled list file: MANIFEST.in
+    include_package_data=True,  # Enabled list file: MANIFEST.in
     install_requires=[
         'numpy>=1.17.0',
         'matplotlib>=3.0.0',
     ],
+    extras_require={
+        "dev": [
+            "yapf>=0.32",
+            "flake8>=4.0",
+            "isort>=5.10",
+            "commitizen>=2.26",
+            "pre-commit>=2.17",
+            "toml>=0.10"
+        ],
+        "test": ["pytest>=7.0"]
+    },
     platforms='any',
-    classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'Topic :: Software Development',
-    'Topic :: Scientific/Engineering',
-    'Topic :: Scientific/Engineering :: Mathematics',
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
-    long_description = LONG_DESCRIPTION,
-    zip_safe = False,
+    long_description=(
+        "Geatpy--The Genetic and Evolutionary Algorithms Toolbox for Python. "
+        "http://www.geatpy.com"),
+    zip_safe=False,
 )
 
-dest_path = 'geatpy/core/'
-source_path = '_core/'
+dest_path = pathlib.Path('geatpy/core/')
+source_path = pathlib.Path('_core/')
 
-def findAndCopy(path):
-    for files in os.listdir(path):
-        name = os.path.join(path, files)
-        if os.path.isfile(name):
-            shutil.copy(name, dest_path + files)
-
-# delete the exist files
-def findAndDel(path):
-    for files in os.listdir(path):
-        name = os.path.join(path, files)
-        if os.path.isfile(name):
-            os.remove(name)
-
-# copy the core files
-if os.path.exists(dest_path) == False:
-    os.makedirs(dest_path)
-versionName = sys.version.split()[0].split('.')
-versionName = versionName[0] + '.' + versionName[1]
-core_path = source_path + platform.system() + '/lib' + platform.architecture()[0][:2] + '/v' + versionName
-findAndDel(dest_path)
-findAndCopy(core_path)
+if dest_path.exists():
+    shutil.rmtree(dest_path)
+dest_path.mkdir()
+python_version = 'v{}.{}'.format(sys.version_info.major,
+                                 sys.version_info.minor)
+core_path = (source_path / '{}'.format(platform.system())
+             / 'lib{}'.format(platform.architecture()[0][:2]) / python_version)
+for file in core_path.iterdir():
+    shutil.copy(file, dest_path / file.name)
 
 try:
-    setup(include_dirs=[np.get_include()],ext_modules=cythonize("build/build.pyx"),language="c", **kwargs),
+    setup(include_dirs=[np.get_include()],
+          ext_modules=cythonize("build/build.pyx"),
+          language="c",
+          **kwargs),
 except Exception:
     setup(**kwargs)
