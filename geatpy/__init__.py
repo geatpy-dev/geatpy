d7177df5b738e727a4785f043271296c1db3aeb3
Brooks YUAN <hktkzyx@yeah.net>
2022-05-18 19:14:39 +0800

    style(geatpy.__init__py): extract benchmarks __init__ to geatpy __init__

--- geatpy/__init__.py
+++ geatpy/__init__.py
@@ -73,7 +73,30 @@ from geatpy.algorithms.soeas.GA.soea_psy_steadyGA_templet import \
 from geatpy.algorithms.soeas.GA.soea_SEGA_templet import soea_SEGA_templet
 from geatpy.algorithms.soeas.GA.soea_studGA_templet import soea_studGA_templet
 # import benchmarks
-import geatpy.benchmarks
+from geatpy.benchmarks.mops.BNH import BNH
+from geatpy.benchmarks.mops.C1_DTLZ1 import C1_DTLZ1
+from geatpy.benchmarks.mops.CF1 import CF1
+from geatpy.benchmarks.mops.Conp import CON
+from geatpy.benchmarks.mops.DTLZ4 import DTLZ4
+from geatpy.benchmarks.mops.Fonseca import Fonseca
+from geatpy.benchmarks.mops.IMOP1 import IMOP1
+from geatpy.benchmarks.mops.OSY import OSY
+from geatpy.benchmarks.mops.SRN import SRN
+from geatpy.benchmarks.mops.TNK import TNK
+from geatpy.benchmarks.mops.UF1 import UF1
+from geatpy.benchmarks.mops.WFG1 import WFG1
+from geatpy.benchmarks.mops.ZDT2 import ZDT2
+from geatpy.benchmarks.sops.Ackley import Ackley
+from geatpy.benchmarks.sops.Beale import Beale
+from geatpy.benchmarks.sops.Goldstein import Goldstein
+from geatpy.benchmarks.sops.Griewangk import Griewangk
+from geatpy.benchmarks.sops.Pathological import Pathological
+from geatpy.benchmarks.sops.Rastrigrin import Rastrigrin
+from geatpy.benchmarks.sops.Rosenbrock import Rosenbrock
+from geatpy.benchmarks.sops.Schwefel import Schwefel
+from geatpy.benchmarks.sops.Shubert import Shubert
+from geatpy.benchmarks.sops.Sphere import Sphere
+from geatpy.benchmarks.tsps.TSP import TSP
 # import the core
 from geatpy.core import indicator
 from geatpy.core.awGA import awGA
